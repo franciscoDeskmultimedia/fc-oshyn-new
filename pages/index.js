ac4e61014e3c02b0897982db9ecd65322fc127ca
Francisco <francisco.deskmultimedia@gmail.com>
2022-12-08 19:10:54 -0500

    adding document

--- pages/index.js
+++ pages/index.js
@@ -355,13 +355,7 @@ export default function Home({homepage, nav}) {
 
         <Script type="text/javascript" src='https://promedica.tfaforms.net/wForms/3.11/js/localization-en_US.js?v=c9ffa70238c7739bfa873c10f586d47adbfab7a2'></Script>
 
-        <link href='https://promedica.tfaforms.net/dist/form-builder/5.0.0/wforms-layout.css?v=c9ffa70238c7739bfa873c10f586d47adbfab7a2' rel="stylesheet" type="text/css" />
-
- 
-
-<link href='https://promedica.tfaforms.net/uploads/themes/theme-24.css' rel="stylesheet" type="text/css" />
-
-<link href='https://promedica.tfaforms.net/dist/form-builder/5.0.0/wforms-jsonly.css?v=c9ffa70238c7739bfa873c10f586d47adbfab7a2' rel="alternate stylesheet" title="This stylesheet activated by javascript" type="text/css" />
+        
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
